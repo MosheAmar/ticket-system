9ebf30bd76bd7a9617bf99d052a7fbe593fb8cf9
משה עמר <mosheamar2@gmail.com>
2025-03-05 22:52:27 +0200

    bug fix

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,22 +5,29 @@ import { useEffect, useState } from "react";
 import Cards from "../cards/page";
 import TableView from "../table/page";
 import AddTicket from "../addTicket/page";
+import { redirect } from "next/navigation";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 export default function Dashboard () {
     const [tickets, setTickets] = useState([]);
     const [isCardDisplay, setIsCardDisplay] = useState(true);
     useEffect(()=> {
         const savedTickets = localStorage.getItem('savedTickets')
-        console.log(JSON.parse(savedTickets))
         if (JSON.parse(savedTickets)) {
             setTickets(JSON.parse(savedTickets))
         }
     }, [])
 
+    const { isAuthenticated, isLoading, user } = useKindeBrowserClient();
+    
+    if (!isAuthenticated && !isLoading) {
+      redirect("/unAuth");
+    }
+
     return (
         <main className="my-7 flex flex-col items-center">
             <div className="flex justify-center gap-5">
-                <h1 className="font-bold text-xl leading-8">Manage your tickets here!</h1>
+                <h1 className="font-bold text-xl leading-8">Hi {user?.given_name}, Manage your tickets here!</h1>
                 <Button variant={'outline'} onClick={()=>setIsCardDisplay(!isCardDisplay)}>{isCardDisplay ? 'Table Display' : 'Cards Display'}</Button>
             </div>
             <div>
