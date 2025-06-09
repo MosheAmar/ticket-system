d8d47e97c181f332222ddf4ac591bdc734a98edc
Moshe Amar <mosheamar2@gmail.com>
2025-06-09 15:09:40 +0300

    bug fix

--- app/(auth)/login/forgot-password/page.jsx
+++ app/(auth)/login/forgot-password/page.jsx
@@ -15,9 +15,11 @@ import { useForm } from "react-hook-form"
 import { z } from "zod";
 import { forgetPassword } from "@/lib/auth-client";
 import { findUser } from "@/app/actions";
-
+import { useState } from "react";
+import Link from "next/link";
 
 export default function ForgotPassword() {
+  const [sentLink, setSentLink] = useState(false);
 
   const formSchema = z.object({
     email: z.string().email({
@@ -37,18 +39,20 @@ export default function ForgotPassword() {
     const user = JSON.parse(data);
 
     if (user) {
-
         const { error } = await forgetPassword({
             email: values.email,
             redirectTo: `${window.location.origin}/login/reset-password`,
         });
+        setSentLink(true);
     } else {
         alert("User not found. Please check your email address.");
     }
   }
     
   return (
-      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
+     <>
+      {!sentLink ? (
+        <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
         <h2 className="text-2xl font-bold text-center mb-6">Forgot Password</h2>
         <p className="text-sm text-gray-600 mb-4">
           Enter your email address to receive a password reset link.
@@ -73,6 +77,19 @@ export default function ForgotPassword() {
                     </Button>
                 </form>
             </Form>
-      </div>
+        </div> 
+        ) : (
+        <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md text-center">
+          <h2 className="text-2xl font-bold mb-4">Check Your Email</h2>
+          <p className="text-sm text-gray-600 mb-4">
+            A password reset link has been sent to your email address.
+            Check your inbox and follow the instructions to reset your password.
+          </p>
+          <Link href="/login" className="text-blue-500 hover:underline">
+            Back to Login
+          </Link>
+        </div>
+        )}
+     </> 
   );
 }
--- app/(auth)/login/reset-password/ResetPassword.jsx
+++ app/(auth)/login/reset-password/ResetPassword.jsx
@@ -13,7 +13,7 @@ import { Input } from "@/components/ui/input"
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form"
 import { z } from "zod";
-import { useSearchParams } from "next/navigation";
+import { redirect, useSearchParams } from "next/navigation";
 import { resetPassword } from "@/lib/auth-client";
 
 export default function ResetPassword() {
@@ -40,6 +40,9 @@ export default function ResetPassword() {
         token,
         newPassword: values.password,
       });
+      setTimeout(() => {
+        redirect("./")
+      }, 2000);
   }
     
   return (
